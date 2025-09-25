62c285bb29b2b71df363208bea174cfd3fae60da
Makram Shmait <makramshmait2004@gmail.com>
2025-09-25 19:53:03 +0300

    your commit message

--- fitness-app/src/App.tsx
+++ fitness-app/src/App.tsx
@@ -1,16 +1,20 @@
-import React from "react";
 import { Box } from "@mui/material";
 import { Route,Routes } from "react-router-dom";
-
+import Navbar from "./components/Navbar";
+import Home from "./page/Home";
+import ExerciseDetail from "./page/ExerciseDetail";
+import Footer from "./components/Footer";
 const App = () => {
   return (
     <>
+    <Home/>
       <Box  className="w-[400px]">
        <Navbar />
         <Routes>
           <Route path="/" element={ <Home />  } />
           <Route path="/exercise/:id" element={<ExerciseDetail />} />
         </Routes>
+        <Footer />
       </Box>
     </>
   )
