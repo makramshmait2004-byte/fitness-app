415d1c1cf95a46c704ff7d87865ee4cedabfe87b
Makram Shmait <makramshmait2004@gmail.com>
2025-09-25 19:44:43 +0300

    your message here

--- fitness-app/vite.config.ts
+++ fitness-app/vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+ import tailwindcss from '@tailwindcss/vite'; // Import the plugin
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [ react(),
+        tailwindcss(), 
+  ],
 })
